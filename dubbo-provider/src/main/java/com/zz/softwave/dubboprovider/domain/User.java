46b9f70f3fd7ebd779cc7ec7cf97c7b620cd7c73
卫刚 张 <1950442495@qq.com>
2023-10-07 21:50:53 +0800

    pom

---
+++ dubbo-provider/src/main/java/com/zz/softwave/dubboprovider/domain/User.java
@@ -0,0 +1,17 @@
+package com.zz.softwave.dubboprovider.domain;
+
+import lombok.Data;
+
+/**
+ * @Describtion: User
+ * @Author: 张卫刚
+ * @Date: 2023/10/7 21:15
+ */
+@Data
+public class User {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+
+}
